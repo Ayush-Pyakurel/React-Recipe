dd86808d22a235e1bfaa78b10d87483b5cc476de
Ayush-Pyakurel <ayushpyakurel449@gmail.com>
2022-11-10 22:35:35 +0545

    Create recipe form added

--- src/Pages/create/Create.js
+++ src/Pages/create/Create.js
@@ -1,7 +1,52 @@
 import './Create.css';
 
+import { useState } from 'react';
+
 const Create = () => {
-  return <div>Create</div>;
+  const [title, setTitle] = useState('');
+  const [method, setMethod] = useState('');
+  const [cookingTime, setCookingTime] = useState('');
+
+  return (
+    <div className='create'>
+      <h2>Add New Recipe: </h2>
+
+      <form>
+        <label>
+          <span>Recipe Title: </span>
+          <input
+            type='text'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        {/* Ingrediants */}
+        <label>
+          <span>Recipe Method: </span>
+          <textarea
+            type='textarea'
+            onChange={(e) => {
+              setMethod(e.target.value);
+            }}
+            value={method}
+            required
+          />
+        </label>
+        <label>
+          <span>Cooking Tim (Mins): </span>
+          <input
+            type='number'
+            onChange={(e) => {
+              setCookingTime(e.target.value);
+            }}
+            value={cookingTime}
+          />
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
