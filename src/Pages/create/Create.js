51327cdb55e01b8abd71b69e587958e4beb2dd1e
Ayush-Pyakurel <ayushpyakurel449@gmail.com>
2022-11-11 14:02:37 +0545

    Add current ingrediants style for the added ingrediants auto focus on the first input field added logic to avoid the duplicate entry of ingrediants

--- src/Pages/create/Create.js
+++ src/Pages/create/Create.js
@@ -1,16 +1,42 @@
 import './Create.css';
 
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [method, setMethod] = useState('');
   const [cookingTime, setCookingTime] = useState('');
+  const [newIngrediants, setNewIngrediants] = useState('');
+  const [ingrediants, setIngrediants] = useState([]);
 
+  const titleRef = useRef(null);
+
+  //auto focus on the first input field immediately the component render
+  useEffect(() => {
+    titleRef.current.focus();
+  }, []);
+
+  //funtion to submit the form
   const handleFormSubmit = (e) => {
     e.preventDefault();
     console.log(method, cookingTime, title);
   };
+
+  //function to add the ingrediants
+  const handleAddIngrediants = (e) => {
+    e.preventDefault();
+    const trimedIngrediants = newIngrediants.trim();
+
+    //as ingrediants should not be duplicated; this is to check wheather the ingrediants are unique or not and updating the new added ingrediants inside array
+    if (trimedIngrediants && !ingrediants.includes(trimedIngrediants)) {
+      setIngrediants((prevIngrediants) => [
+        ...prevIngrediants,
+        trimedIngrediants,
+      ]);
+    }
+    setNewIngrediants('');
+  };
+
   return (
     <div className='create'>
       <h2 className='page-title'>Add New Recipe: </h2>
@@ -23,10 +49,30 @@ const Create = () => {
             onChange={(e) => setTitle(e.target.value)}
             value={title}
             required
+            ref={titleRef}
           />
         </label>
-
-        {/* Ingrediants */}
+        <>
+          <label>
+            <span>Recipe Ingrediants: </span>
+            <div className='ingrediants'>
+              <input
+                onChange={(e) => {
+                  setNewIngrediants(e.target.value);
+                }}
+                value={newIngrediants}
+                required
+              />
+              <button onClick={handleAddIngrediants}>Add</button>
+            </div>
+          </label>
+          <p>
+            Current Ingrediants:{' '}
+            {ingrediants.map((ingrediant) => (
+              <em key={ingrediant}>{ingrediant},</em>
+            ))}
+          </p>
+        </>
         <label>
           <span>Recipe Method: </span>
           <textarea
