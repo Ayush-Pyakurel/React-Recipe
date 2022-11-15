56d0921c405c16e93ce24007b48df873ca033912
Ayush-Pyakurel <ayushpyakurel449@gmail.com>
2022-11-15 17:51:53 +0545

    ModeChange action added to the reducer in context dark/light mode toggle icon added style to dark/light mode toggle icon

--- src/Component/ThemeSelector.js
+++ src/Component/ThemeSelector.js
@@ -1,16 +1,30 @@
 //import css
 import './ThemeSelector.css';
 
+//import icon
+import modeIcon from '../assets/mode-icon.svg';
+
 //import custome hook
 import { useTheme } from '../hooks/useTheme';
 
 const themeColors = ['#58249c', '#249c6b', '#b70233'];
 
 const ThemeSelector = () => {
-  const { changeColor } = useTheme();
+  const { changeColor, changeMode, mode } = useTheme();
+
+  const handleModeChange = () => {
+    changeMode(mode === 'dark' ? 'light' : 'dark');
+  };
 
   return (
     <div className='theme-selector'>
+      <div className='toggle-mode'>
+        <img
+          src={modeIcon}
+          alt='mode-toggling-icon'
+          onClick={handleModeChange}
+        />
+      </div>
       <div className='theme-buttons'>
         {themeColors.map((color) => {
           return (
