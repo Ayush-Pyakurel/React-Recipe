e1cf47b4c573ca9551f78c4129b2756e220fd2c0
Ayush Pyakurel <ayushpyakurel449@gmail.com>
2022-11-18 07:26:06 +0545

    Installed firebase added firebase config and initialized firebase and fireStore

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore ';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCapwE2XHECI7nfhElTPa814__RV3piHbE',
+  authDomain: 'cooking-recipe-directory-262ef.firebaseapp.com',
+  projectId: 'cooking-recipe-directory-262ef',
+  storageBucket: 'cooking-recipe-directory-262ef.appspot.com',
+  messagingSenderId: '19046188371',
+  appId: '1:19046188371:web:1bc7f3d84f690badb44da6',
+};
+
+//initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+//initialize firestore
+const projectFireStore = firebase.firestore();
