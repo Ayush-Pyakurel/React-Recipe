12550fad76900cc233941a574865297f11f73760
Ayush-Pyakurel <ayushpyakurel449@gmail.com>
2022-11-21 17:49:08 +0545

    setup to update individual recipe

--- src/Pages/recipe/Recipe.js
+++ src/Pages/recipe/Recipe.js
@@ -5,14 +5,35 @@ import './Recipe.css';
 import { useParams } from 'react-router-dom';
 
 //costume hook imports
-import { useFetch } from '../../hooks/useFetch';
+// import { useFetch } from '../../hooks/useFetch';
 import { useTheme } from '../../hooks/useTheme';
+
+//import react hooks
+import { useState, useEffect } from 'react';
+
+//import firestore
+import { projectFirestore } from '../../firebase/config';
 
 export default function Recipe() {
   //accessing the route parameter; and whenever we go to particular recipe id of the recipe will be attached to url
   const { id } = useParams();
-  const url = `http://localhost:3000/recipes/${id}`;
-  const { data: recipe, error, loading } = useFetch(url);
+  // const url = `http://localhost:3000/recipes/${id}`;
+  // const { data: recipe, error, loading } = useFetch(url);
+
+  const [recipe, setRecipe] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    projectFirestore
+      .collection('recipes')
+      .doc(id)
+      .get()
+      .then((doc) => {
+        setLoading(true);
+        console.log(doc);
+      });
+  }, [id]);
 
   const { mode } = useTheme();
 
